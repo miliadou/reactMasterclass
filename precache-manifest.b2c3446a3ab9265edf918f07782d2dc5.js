a4a01fe923f22f0586f1db583ff7fff086abd960
DESKTOP-DKPEC7G\mIliadoulaptop <m.iliadou93@yahoo.com>
2019-09-28 17:35:02 +0300

    Updates

---
+++ precache-manifest.b2c3446a3ab9265edf918f07782d2dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58836c4e34b11a491183c32576a7e42b",
+    "url": "/reactMasterclass/index.html"
+  },
+  {
+    "revision": "71014aeac50d72829994",
+    "url": "/reactMasterclass/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a8b310b8cb6d430a4486",
+    "url": "/reactMasterclass/static/js/2.1caea5c3.chunk.js"
+  },
+  {
+    "revision": "71014aeac50d72829994",
+    "url": "/reactMasterclass/static/js/main.a1b95f41.chunk.js"
+  },
+  {
+    "revision": "09de751ae09c818e7e1b",
+    "url": "/reactMasterclass/static/js/runtime-main.971593c6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactMasterclass/static/media/logo.25bf045c.svg"
+  }
+]);
